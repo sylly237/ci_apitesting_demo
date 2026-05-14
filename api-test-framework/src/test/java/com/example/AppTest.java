491504faa6b864fd1e30a389d6f475a4c2112a23
sylly237 <85211551+sylly237@users.noreply.github.com>
2026-05-14 01:36:56 -0400

    add simple mock api test with stubbed response

---
+++ api-test-framework/src/test/java/com/example/AppTest.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import static io.restassured.RestAssured.*;
+import static org.hamcrest.Matchers.*;
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import org.junit.jupiter.api.*;
+
+public class AppTest {
+
+    private static WireMockServer wireMockServer;
+
+    @BeforeAll
+    static void setup() {
+        wireMockServer = new WireMockServer(8080);
+        wireMockServer.start();
+
+        configureFor("localhost", 8080);
+    }
+
+    @AfterAll
+    static void teardown() {
+        wireMockServer.stop();
+    }
+
+    @Test
+    public void shouldGetUserSuccessfully() {
+
+        stubFor(get(urlEqualTo("/users/1"))
+            .willReturn(aResponse()
+                .withStatus(200)
+                .withHeader("Content-Type", "application/json")
+                .withBody("""
+                    {
+                      "id": 1,
+                      "name": "John Doe"
+                    }
+                """)));
+
+        given()
+            .baseUri("http://localhost:8080")
+        .when()
+            .get("/users/1")
+        .then()
+            .statusCode(200)
+            .body("name", org.hamcrest.Matchers.equalTo("John Doe"));
+    }
+}
